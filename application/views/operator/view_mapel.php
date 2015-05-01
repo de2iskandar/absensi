c4eaef41d8a33e4e34fd62102bf991dfadc84adf
Dede Iskandar <dede.iskandar@student.uinsgd.ac.id>
2015-05-01 20:05:13 +0700

    fix view mapel

--- application/views/operator/view_mapel.php
+++ application/views/operator/view_mapel.php
@@ -43,19 +43,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	</section>
 	<section id="content">
 		<div class="container">
-			<?php if ($this->session->flashdata('tambah')) : ?>
-			<div class="alert alert-success alert-dismissible" role="alert">
-  				<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-  				<strong>Sukses!</strong> data berhasil ditambahkan.
-			</div>
-    		<?php endif ?>
-
-    		<?php if ($this->session->flashdata('update')) : ?>
-    		<div class="alert alert-info alert-dismissible" role="alert">
-  				<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-  				<strong>Info!</strong> data berhasil diperbarui.
-			</div>
-    		<?php endif ?>
 
     		<?php if ($this->session->flashdata('delete')) : ?>
     		<div class="alert alert-danger alert-dismissible" role="alert">
@@ -63,33 +50,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   				<strong>Warning!</strong> data telah dihapus.
 			</div>
     		<?php endif ?>
-
-    		<?php if ($this->session->flashdata('import')) : ?>
-    		<div class="alert alert-danger alert-dismissible" role="alert">
-  				<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-  				<strong>Sukses!</strong> data telah diimport.
-			</div>
-    		<?php endif ?>
     		
 			<div class="panel panel-default">
 				<div class="panel-heading">
 					<div class="row">
 						<div class="col-xs-12 col-md-8">
-							<h4>Daftar Siswa</h4>
+							<h4>Daftar Mata Pelajaran</h4>
 						</div>
-  						<div class="col-xs-6 col-md-4 text-right">
-  							<a href="<?php echo base_url()."operator/import_siswa"; ?>">
-								<button type="button" class="btn btn-success btn-sm">Import</button>
-							</a>
-
-							<a href="<?php echo base_url()."operator/export_siswa"; ?>">
-								<button type="button" class="btn btn-success btn-sm">Export</button>
-							</a>
-
-  							<a href="<?php echo base_url()."operator/add_siswa"; ?>">
-								<button type="button" class="btn btn-success btn-sm">Tambah</button>
-							</a>
-  						</div>
 					</div>
 			</div>
 				<div class="panel-body">
